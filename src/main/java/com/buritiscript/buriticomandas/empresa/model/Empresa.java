fffeb86ac023293ce4410925ee119ed5023bd7f6
Emerson Reis da Costa <emerson.costa@pmbv.local>
2024-04-03 12:02:56 -0400

    criado entidade Empresa

---
+++ src/main/java/com/buritiscript/buriticomandas/empresa/model/Empresa.java
@@ -0,0 +1,39 @@
+package com.buritiscript.buriticomandas.empresa.model;
+
+
+import org.hibernate.validator.constraints.br.CNPJ;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Empresa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long codigo;
+    @NotBlank
+    @NotNull
+    private String nomeFantasia;
+    @NotBlank
+    @NotNull
+    private String razaoSocial;
+    private String idContato;
+    private String idEndereco;
+    @NotBlank
+    @CNPJ
+    private String cnpj;
+
+    @Deprecated
+    public Empresa() {
+    }
+
+    
+}
