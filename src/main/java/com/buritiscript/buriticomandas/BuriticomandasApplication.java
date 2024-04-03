51e71bbc1381a12c6c7dd94a31d8ea46e7c6af10
Emerson Reis da Costa <emerson.costa@pmbv.local>
2024-04-03 11:05:16 -0400

    Projeto criado

---
+++ src/main/java/com/buritiscript/buriticomandas/BuriticomandasApplication.java
@@ -0,0 +1,13 @@
+package com.buritiscript.buriticomandas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuriticomandasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuriticomandasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buritiscript/buriticomandas/BuriticomandasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buritiscript.buriticomandas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuriticomandasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
